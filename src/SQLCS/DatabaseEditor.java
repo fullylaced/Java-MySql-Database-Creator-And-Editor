f50a16229f835e14fc54f96c0ded8f1b04ff744b
fullylaced <89863341+fullylaced@users.noreply.github.com>
2023-09-20 13:59:05 -0400

    Functionality Updates
    Includes ability to name database and connect to specific server without editing

--- src/SQLCS/DatabaseEditor.java
+++ src/SQLCS/DatabaseEditor.java
@@ -9,43 +9,67 @@ package SQLCS;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
 public class DatabaseEditor {
-	static String NO_LINK = "\"N/A\"";
-	static String LOCALHOST_STRING = "localhost:3306/?useSSL=false";
-	
+    // Constants for default values
+    static String NO_LINK = "\"N/A\"";
+    static String LOCALHOST_STRING = "localhost:3306/?useSSL=false";
+
+    // Method to establish a database connection
+    public static Connection connect() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the MySQL Server Link (or type " + NO_LINK + " or leave entry blank to reach localhost):");
+        String linkEntry = scanner.nextLine();
+        Connection con = null;
+        if (linkEntry.equals("N/A") || linkEntry.isEmpty()) {
+            // Connect to localhost if no link is provided
+            con = DriverManager.getConnection(("jdbc:mysql://" + LOCALHOST_STRING), "root", "");
+        } else {
+            // Connect to the provided link
+            con = DriverManager.getConnection(("jdbc:mysql://" + linkEntry), "root", "");
+        }
+        return con;
+    }
+
+    // Method to create a new database
+    public static int createDatabase(Statement stmt) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What would you like the name of the Database to be?: ");
+        String nameEntry = scanner.nextLine();
+
+        return stmt.executeUpdate("CREATE DATABASE " + nameEntry);
+    }
+
     public static void main(String[] args) {
         // Initialize variables for database connection and statement
         Connection con = null;
         Statement stmt = null;
-        
-        // Specify the name of the database you want to create
-        String yourDatabaseName = "Test_Database";
-        
+
         try {
-        	Scanner scanner = new Scanner(System.in);
-        	System.out.println("Enter the MySQL Server Link (or type " + NO_LINK + " or leave entry blank to reach localhost):");
-        	String linkEntry = scanner.nextLine();
-        	if (linkEntry.equals("N/A") || linkEntry.isEmpty()) {
-        		con = DriverManager.getConnection(("jdbc:mysql://" + LOCALHOST_STRING), "root", "");
-        	}else {
-        		con = DriverManager.getConnection(("jdbc:mysql://" + linkEntry), "root", "");
-        	}
-        	scanner.close();
-            // Establish a connection to the MySQL server
-            
+            int status = 0;
+            con = connect();
+            System.out.println("Connected to MySQL Server!");
+
             // Create a statement object to execute SQL commands
             stmt = con.createStatement();
-            
-            // Execute an SQL command to create a new database with the specified name
-            int status = stmt.executeUpdate("CREATE DATABASE " + yourDatabaseName);
-            
+            System.out.println("What would you like to do within the SQL Database?");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter 1 to Create a Database");
+            String choiceEntry = scanner.nextLine();
+
+            if (choiceEntry.equals("1")) {
+                // Call the createDatabase method if the user chooses to create a database
+                status = createDatabase(stmt);
+            }
+
             // Check if the database creation was successful
             if (status > 0) {
                 System.out.println("Database is created successfully !!!");
             }
+            scanner.close();
         } catch (Exception e) {
             // Handle any exceptions that may occur during database creation
             e.printStackTrace();
